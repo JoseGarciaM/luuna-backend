7542a65dee125ea0ecd7e1e922202c4837699849
Jose Carlos Garcia Mendizabal <info@josegarciam.com>
2021-09-30 22:56:56 -0500

    added home section schema for testing

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,24 +1,26 @@
 import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import blockContent from './blockContent';
+import localeString from './locale/String';
+import localeText from './locale/Text';
+import localeBlockContent from './locale/BlockContent';
 import category from './category';
 import subcategory from './subcategory';
 import product from './product';
 import productVariant from './productVariant';
-import localeString from './locale/String';
-import localeText from './locale/Text';
-import localeBlockContent from './locale/BlockContent';
+import sectionHome from './sectionHome';
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    product,
-    category,
-    subcategory,
     blockContent,
     localeText,
     localeBlockContent,
     localeString,
     productVariant,
+    product,
+    category,
+    subcategory,
+    sectionHome,
   ]),
 })
